3c7824cd5d2e1d62b2910fe5cb7d1fa2ec250e9b
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-05-19 02:32:02 +0600

    data loaded

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// using middletier
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://todo-user:lfWlplLC7qRiWKRb@cluster0.uucsy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const todoCollection = client.db("todolist").collection("works");
+
+        app.get('/posts', async (req, res)=>{
+            const query = {};
+            const cursor = todoCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
